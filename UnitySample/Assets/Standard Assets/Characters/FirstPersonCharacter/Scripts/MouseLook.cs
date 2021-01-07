8a93262c4c173c61e3015c133f186b15c4499550
Prasanth Sasikumar <prasanths99@yahoo.co.in>
2021-01-08 02:02:12 +1300

    added chem lab and teleportation

--- UnitySample/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
+++ UnitySample/Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/MouseLook.cs
@@ -111,5 +111,20 @@ namespace UnityStandardAssets.Characters.FirstPerson
             return q;
         }
 
+        //NEW METHOD ADDED
+        public bool Mouselocked()
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+            {
+                //Debug.Log("Locked");
+                return true;
+            }
+            else
+            {
+                //Debug.Log("unLocked");
+                return false;
+            }
+        }
+
     }
 }
